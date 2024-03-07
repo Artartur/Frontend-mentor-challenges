fc127745c6df32d89893fc7e39bcf2b919fbd98b
artartur <arturgfs@hotmail.com>
2024-03-07 01:16:31 -0300

    Validate email and check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
-export class AppComponent { }
+export class AppComponent {
+  email: string = "";
+  isEmailValid: boolean = true;
+  constructor() {}
+
+  changeBorderColor() {
+    return this.email == ""
+      ? "border-secondary"
+      : this.email !== "" && !this.isEmailValid
+      ? "bg-danger-subtle border-danger text-danger-emphasis"
+      : "border-success";
+  }
+
+  onSubmit() {
+    return this.isEmailValid ? console.log("true") : console.log("false");
+  }
+
+  validateEmail() {
+    var regex = /\S+@\S+\.\S+/;
+    this.isEmailValid = regex.test(this.email);
+  }
+}
