95ee3996e85e2968f0740057ef6c0814c7467183
artartur <artur.souza@tds.company>
2024-03-07 15:23:49 -0300

    Create success component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FormComponent } from './form/form.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { SuccessComponent } from "./success/success.component";
+import { FormComponent } from "./form/form.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-  ],
+  declarations: [AppComponent, SuccessComponent, FormComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
