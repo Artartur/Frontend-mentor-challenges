820e35e858140a2e7f6d05b8cdf78d015f4f7280
artartur <arturgfs@hotmail.com>
2024-04-07 05:16:03 -0300

    feat: add bg image and add header component on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import Header from "./components/Header";
+import "./styles/home.scss";
+
 export default function Home() {
   return (
-   <>
-   </>
+    <div className="home__container bg-cover bg-no-repeat flex h-screen justify-center w-screen">
+      <Header />
+    </div>
   );
 }
