9db38d8bc1454666edb8aa1679713eeb02d05566
artartur <arturgfs@hotmail.com>
2024-04-08 01:42:23 -0300

    refactor: header component and add tablet responsive design

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Logo from "../../assets/shared/logo.svg";
 import Menu from "../../assets/shared/icon-hamburger.svg";
@@ -9,53 +9,66 @@ import "../styles/header.scss";
 
 export default function Header() {
   const [isShowModal, setIsShowModal] = useState(false);
+  const [showMenuIcon, setShowMenuIcon] = useState(true);
 
   const openModal = () => setIsShowModal(!isShowModal);
 
-  return (
-    <header className="header__container items-center flex h-20 justify-center w-full">
-      <div className="flex items-center justify-between w-full">
-        <Image alt="logo" className="ms-5" src={Logo} />
-        <Image
-          alt="close"
-          className={
-            isShowModal
-              ? "absolute cursor-pointer right-5 z-10"
-              : "cursor-pointer me-5"
-          }
-          src={isShowModal ? Close : Menu}
-          onClick={openModal}
-        />
-      </div>
+  useEffect(() => {
+    const handleResize = () => {
+      setShowMenuIcon(window.innerWidth <= 640);
+    };
 
-      {isShowModal && (
-        <nav>
-          <div className="nav-text absolute h-screen right-0 top-0 w-60">
-            <ul className="absolute h-screen right-0 top-0 w-60">
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <header className="header__container items-center flex justify-center w-full">
+      <div className="header__container-content flex items-center justify-between w-full">
+        <Image src={Logo} alt="logo" className="ms-5" />
+
+        {showMenuIcon && (
+          <Image
+            alt="Open the menu to navigate"
+            className={
+              isShowModal
+                ? "absolute top-7 right-5 z-10 cursor-pointer"
+                : "me-5 cursor-pointer"
+            }
+            onClick={openModal}
+            src={isShowModal ? Close : Menu}
+          />
+        )}
+
+        {(isShowModal || !showMenuIcon) && (
+          <nav className="nav-text absolute top-0 right-0 h-full tablet:h-20 tablet:flex tablet:justify-center">
+            <ul className="mt-20 tablet:flex tablet:items-center tablet:mt-0">
               <li>
-                <a href="#">
-                  <strong>00</strong> HOME
+                <a href="">
+                  <strong>00</strong>HOME
                 </a>
               </li>
               <li>
-                <a href="#">
-                  <strong>01</strong> DESTINATION
+                <a href="">
+                  <strong>01</strong>DESTINATION
                 </a>
               </li>
               <li>
-                <a href="#">
-                  <strong>02</strong> CREW
+                <a href="">
+                  <strong>02</strong>CREW
                 </a>
               </li>
               <li>
-                <a href="#">
-                  <strong>03</strong> TECHNOLOGY
+                <a href="">
+                  <strong>03</strong>TECHNOLOGY
                 </a>
               </li>
             </ul>
-          </div>
-        </nav>
-      )}
+          </nav>
+        )}
+      </div>
     </header>
   );
 }
