8bd0385f6c205516ab59c187e5dff2ab31bf5d00
artartur <artur.souza@tds.company>
2024-04-25 22:47:44 -0300

    feat: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./styles/globals.scss";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "News Homepage",
+  description: "A news homepage for you",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
