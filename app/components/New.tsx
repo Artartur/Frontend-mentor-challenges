0972a19fc37e666474ca8ea5da12b17221860bf9
artartur <artur.souza@tds.company>
2024-04-27 03:14:22 -0300

    feat: create New component

---
+++ app/components/New.tsx
@@ -0,0 +1,35 @@
+export default function New() {
+  return (
+    <div className="new__container bg-slate-900 mb-10 desktop:mb-0">
+      <span className="new__container-title">
+        <h2 className="mt-3 ms-4 mb-4">New</h2>
+      </span>
+      <span className="new__container-topics">
+        <h3 className="mb-1 ms-4">Hydrogen VS Electric Cars</h3>
+        <p className="ms-4 mb-3">
+          Will hydrogen-fueled cars ever catch up to EVs?
+        </p>
+      </span>
+
+      <div className="new__container-separator bg-slate-700 mb-3 ms-4 mt-5"></div>
+
+      <span className="new__container-topics pb-3">
+        <h3 className="mb-1 ms-4">The Downsides of AI Artistry</h3>
+        <p className="ms-4 mb-3">
+          What are the possible adverse effects of on-demand AI image
+          generation?
+        </p>
+      </span>
+
+      <div className="new__container-separator bg-slate-700 mb-3 ms-4 mt-5"></div>
+
+      <span className="new__container-topics pb-3">
+        <h3 className="mb-1 ms-4">Is VC Funding Drying Up?</h3>
+        <p className="ms-4 mb-5">
+          Private funding bu VC firms is down 50% YDY. We take a look at what
+          that means.
+        </p>
+      </span>
+    </div>
+  );
+}
