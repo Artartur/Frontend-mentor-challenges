771fbf52bf99c65e2236a09cc39a1943ca5282a9
artartur <artur.souza@tds.company>
2024-04-27 03:15:13 -0300

    feat: create home page and add its components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import New from "./components/New";
+import Tranding from "./components/Tranding";
+import MainTopic from "./components/MainTopic";
+import MainTopicImage from "./components/MainTopicImage";
+import "./styles/home.scss";
+
 export default function Home() {
   return (
-    <main>
-
+    <main className="home__container flex flex-col items-center justify-center mt-2">
+      <div className="home__container-parent grid grid-cols-1 desktop:grid-rows-3">
+        <MainTopicImage />
+        <MainTopic />
+        <New />
+        <Tranding />
+      </div>
     </main>
   );
 }
