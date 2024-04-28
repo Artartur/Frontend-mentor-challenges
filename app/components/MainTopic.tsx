cac3edbb076090a7ec2854f7235859c990d94021
artartur <artur.souza@tds.company>
2024-04-27 23:28:09 -0300

    feat: sort alphabetically

--- app/components/MainTopic.tsx
+++ app/components/MainTopic.tsx
@@ -1,6 +1,6 @@
 export default function MainTopic() {
   return (
-    <div className="mainTopic__texts flex flex-col desktop:flex-row mb-10 desktop:mt-2">
+    <div className="mainTopic__texts flex flex-col mb-10 desktop:flex-row desktop:mt-2">
       <h1 className="mb-2 desktop:mb-0 desktop:me-10">
         The Bright future of Web 3.0?
       </h1>
@@ -10,7 +10,7 @@ export default function MainTopic() {
           power of the platforms back into the hands of the people. But is it
           really fulfilling its promise?
         </p>
-        <button className="outline-none border-none flex items-center justify-center rounded-sm">
+        <button className="border-none flex items-center justify-center outline-none rounded-sm">
           READ MORE
         </button>
       </div>
--- app/components/New.tsx
+++ app/components/New.tsx
@@ -2,11 +2,11 @@ export default function New() {
   return (
     <div className="new__container bg-slate-900 mb-10 desktop:mb-0">
       <span className="new__container-title">
-        <h2 className="mt-3 ms-4 mb-4">New</h2>
+        <h2 className="mb-4 ms-4 mt-3">New</h2>
       </span>
       <span className="new__container-topics">
         <h3 className="mb-1 ms-4">Hydrogen VS Electric Cars</h3>
-        <p className="ms-4 mb-3">
+        <p className="mb-3 ms-4">
           Will hydrogen-fueled cars ever catch up to EVs?
         </p>
       </span>
@@ -15,7 +15,7 @@ export default function New() {
 
       <span className="new__container-topics pb-3">
         <h3 className="mb-1 ms-4">The Downsides of AI Artistry</h3>
-        <p className="ms-4 mb-3">
+        <p className="mb-3 ms-4">
           What are the possible adverse effects of on-demand AI image
           generation?
         </p>
@@ -25,7 +25,7 @@ export default function New() {
 
       <span className="new__container-topics pb-3">
         <h3 className="mb-1 ms-4">Is VC Funding Drying Up?</h3>
-        <p className="ms-4 mb-5">
+        <p className="mb-5 ms-4">
           Private funding bu VC firms is down 50% YDY. We take a look at what
           that means.
         </p>
